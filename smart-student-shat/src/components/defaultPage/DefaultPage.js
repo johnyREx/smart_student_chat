44a762b07d29846b27c8f83818e58de099d8bf73
johnyREx <johnkwofie99@icloud.com>
2024-06-13 10:46:02 +0200

    implemented the default page

---
+++ smart-student-shat/src/components/defaultPage/DefaultPage.js
@@ -0,0 +1,11 @@
+import "./defaultPage.css";
+
+function DefaultPage() {
+    return (
+        <div className="default-chat">
+            <p>Select a Chat to start messaging</p>
+        </div>
+    );
+}
+
+export default DefaultPage;
