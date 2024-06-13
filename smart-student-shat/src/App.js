e8a430f5d8232b289678e8157d3feaeeee125afa
johnyREx <johnkwofie99@icloud.com>
2024-06-13 11:16:16 +0200

    worked on the app itself the moment of truth

--- smart-student-shat/src/App.js
+++ smart-student-shat/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Chatlist from "./components/list/List";
+import Chats from "./components/chat/chat";
+import Detail from "./components/detail/detail";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import DefaultPage from "./components/defaultPage/DefaultPage";
+import { useEffect, useState } from "react";
+import { useUserStore } from "./db/userStore";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, UseAuth } from "./db/firebase";
+import { useChatStore } from "./db/chatStore";
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 function App() {
+  const [login, setLogin] = useState(false);
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {isLoading ? (
+        <Box padding="1" boxShadow="lg"  width="90vw" height="100%">
+          <SkeletonCircle size="10" />
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+          <SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+        </Box>
+       ) : (
+        <>
+          {currentUser ? (
+            <>
+              <Sidebar />
+              <Chatlist />
+              {chatId ? (
+                <>
+                  <Chats />
+                  <Detail />
+                </>
+              ) : (
+                <DefaultPage />
+              )}
+            </>
+          ) : (
+            <Login />
+          )}
+        </>
+      )}
+      <Notification />
     </div>
   );
+  
 }
 
 export default App;
